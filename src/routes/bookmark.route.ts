1dfcf27e6c1ade05f477910dc489c06f66a65611
Hemit Patel <hemitvpatel@gmail.com>
2025-03-22 23:29:04 -0400

    added all routes

--- src/routes/bookmark.route.ts
+++ src/routes/bookmark.route.ts
@@ -1,4 +1,11 @@
 import express from "express"
+import { authenticateSession } from "../middleware/session.middleware";
+import { bookmarkController } from "../controllers/bookmark.controller";
 
 const router = express.Router()
 
+router.get("/get", authenticateSession, bookmarkController.getUserBookmark)
+router.post("/create", authenticateSession, bookmarkController.createBookmark)
+router.delete("/delete", authenticateSession, bookmarkController.deleteBookmark)
+
+export default router;
