876e43bcc0739372d186ffd3db2eaa0f1fe31d0d
Hemit Patel <hemitvpatel@gmail.com>
2025-03-30 13:33:21 -0400

    added /admin/create-question endpoint

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -1,6 +1,9 @@
 import express from "express"
+import { authenticateAdminSession } from "../middleware/session.middleware.ts";
+import adminController from "../controllers/admin.controller.ts";
 
 const router = express.Router();
 
+router.post("/create-question", authenticateAdminSession, adminController.createQuestion)
 
 export default router;
