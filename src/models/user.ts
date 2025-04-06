861d6b839caf2e6083f49a88162a86a95ba26a21
Hemit Patel <hemitvpatel@gmail.com>
2025-04-05 22:30:29 -0400

    made name unique as its being used as an identifer now

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,7 +2,7 @@ import { pgTable, text } from "drizzle-orm/pg-core";
 
 const user = pgTable("user", {
     email: text("email").primaryKey(),
-    name: text("name").notNull(),
+    name: text("name").notNull().unique(),
     role: text("role").notNull(),
 });
 
