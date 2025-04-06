6669223abc43e180787f977442905999a902ca98
Hemit Patel <hemitvpatel@gmail.com>
2025-04-06 00:24:18 -0400

    update

--- src/middleware/session.middleware.ts
+++ src/middleware/session.middleware.ts
@@ -16,6 +16,8 @@ export const authenticateSession = async (
     return res.status(401).json({
       message:
         "You are currently not authenticated! Please log in and try again.",
+      cookie: req.cookies["session-id"]
+
     });
   }
 };
