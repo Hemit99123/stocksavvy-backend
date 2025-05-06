1ff2073a26a5c9c2fb616378050185361cd24a88
Hemit Patel <hemitvpatel@gmail.com>
2025-05-05 20:50:27 -0400

    all tests are units

--- src/__tests__/date.test.ts
+++ src/__tests__/date.test.ts
@@ -1,4 +1,4 @@
-import { handleSanitizeDate } from "../../utils/date/index.ts"
+import { handleSanitizeDate } from "../utils/date/index.ts"
 
 test('proper date sanitization', () => {
   expect(handleSanitizeDate("2020-05-14T04:00:00Z")).toBe("5/14/2020");
